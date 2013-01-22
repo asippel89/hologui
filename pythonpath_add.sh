84987503856d52901eb00a0f44cb7db3a58144fb
Alex Sippel <asippel89@gmail.com>
2013-01-22 17:24:48 -0600

    removed .py from script names

---
+++ pythonpath_add.sh
@@ -0,0 +1,21 @@
+#!/usr/bin/env bash
+
+if [[ "${BASH_SOURCE[0]}" == $0 ]] 
+then
+    echo "Must run by source'ing the script" 
+    echo "\"source $0\""
+fi
+
+unique_env_path ()
+{
+    echo "${2//$1/}"
+}
+
+env_prepend()
+{
+    eval export "$1="\"$2:\"\$\("unique_env_path \"$2:\"" \$$1\)
+}
+
+PATHNAME=$(readlink -f $(dirname "${BASH_SOURCE[0]}"))
+env_prepend PYTHONPATH $PATHNAME
+
